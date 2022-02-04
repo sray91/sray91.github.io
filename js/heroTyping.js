327c6d0c21f34f5503cccf50fcb686512a5ef7b6
swanagan <swanaganray@gmail.com>
2022-02-03 22:13:54 -0500

    trying to get this gd javascript to render

--- js/heroTyping.js
+++ js/heroTyping.js
@@ -1,19 +1,2 @@
-var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
-
-document.addEventListener("DOMContentLoaded", function() {
-    // this function runs when the DOM is ready, i.e. when the document has been parsed
-    if (i < txt.length) {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    function typeWriter() {
-      if (i < txt.length) {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-      }
-    }
-  }
-});
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
