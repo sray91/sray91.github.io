e8ff83389d5b91898c607b63661ee33111b3ebcb
swanagan <swanaganray@gmail.com>
2022-03-23 00:38:55 -0400

    finally got the wordpress api working!

--- js/blogPosts.js
+++ js/blogPosts.js
@@ -1,42 +1,19 @@
 const apiURL = 'https://eewatchguy.com/wp-json/wp/v2/posts?per_page=1';
-const webURL = 'https://eewatchguy.com';
 
-async function fetchPosts() {
-    try {
-        const response = await fetch(`${apiURL}`, { method: 'GET'});
+async function listPost(postContainerElementID) {
 
-        if (!response.ok) {
-            throw new Error(`Failed to fetch post: ${response.status}`)
-        }
-        return await response.json();
-    } catch(e) {
-        console.log(e);
-    }
+    fetch('https://eewatchguy.com/wp-json/wp/v2/posts?per_page=1')
+    .then( response => response.json())
+    .then( response => {
+        console.log(response[0])
+        let data1 = "";
+        response.map((values) => {
+            data1 = `<h3>${values.title.rendered}</h3><p>${values.excerpt.rendered}</p><button><a href=${values.link}</button>`
+        })
+        document.getElementById(postContainerElementID).innerHTML = data1;
+    }).catch((err) => {
+        console.log(err);
+    });
 }
 
-function listPost(postContainerElementID) {
-    const postContainerElement = document.getElementById(postContainerElementID);
-
-    if (!postContainerElement) {
-        return;
-    }
-
-    fetchPosts()
-    .then(posts => {
-        if (!posts) {
-            postContainerElement.innerHTML = 'No posts available';
-            return;
-        }
-        postContainerElement.appendChild(postElement(posts));
-    })
-    .catch(e => {
-        console.log(e);
-    })
-}
-
-function postElement(post) {
-    const titleElement = document.createElement('h2');
-    const bodyElement = document.createElement('p');
-    titleElement.innerHTML = post.title.rendered;
-    bodyElement.innerHTML = post.content.rendered;
-} 
+listPost('blog-posts')
