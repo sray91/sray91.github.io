32417d1cabd05158524feff9e3c0aa71689fcba0
swanagan <swanaganray@gmail.com>
2022-04-05 23:41:32 -0400

    finalizing blog post section

--- js/blogPosts.js
+++ js/blogPosts.js
@@ -5,23 +5,23 @@ async function listPost(postContainerElementID) {
     fetch('https://eewatchguy.com/wp-json/wp/v2/posts?per_page=1')
     .then( response => response.json())
     .then( response => {
-        console.log(response[0])
-        let data1 = "";
+        let data = "";
         response.map((values) => {
-            data1 = `
+            data = `
             <a href=${values.link} target="_blank">
                 <figure class="blog-post-figure">
-                    <img class="blog-post-img" src="./images/eeWatchGuyLogoAngle.png" alt="" width="200" height="200"></img> 
+                    <div class="blog-post-img" >
+                        <img src="./images/eeWatchGuyLogoAngle.png" alt="" width="200" height="200"></img>
+                    </div>
                     <div class="blog-post-container">
-                        <h3 class="blog-post-content">${values.title.rendered}</h3>
-                        <p class="blog-post-content">${values.date}</p>
-                        <p class="blog-post-content">${values.excerpt.rendered}</p>
+                        <h3 class="blog-post-content-h3">${values.title.rendered}</h3>
+                        <p class="blog-content">${values.excerpt.rendered}</p>
                     </div>
                 </figure>
             </a>
             `
         })
-        document.getElementById(postContainerElementID).innerHTML = data1;
+        document.getElementById(postContainerElementID).innerHTML = data;
     }).catch((err) => {
         console.log(err);
     });
