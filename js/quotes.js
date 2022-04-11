9062aaca7212ef3a5ddb0f62efeabb9be9090b4b
swanagan <swanaganray@gmail.com>
2022-04-10 20:54:00 -0400

    finished up the quotes stuff

--- js/quotes.js
+++ js/quotes.js
@@ -1,18 +1,27 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 async function loadQuotes(postContainerElementID) {
+    let dataList = new Array();
     fetch("../content/quotes.json")
-        .then(Response => Response.json())
+        .then(Response => Response.json()) 
         .then(Response => {
+            
             let data = "";
-            Response.map((values) => {
+            let startNum = getRandomInt(Response.length);
+            (startNum >= (Response.length-5)) ? (startNum-=5) : null;
+            let endNum = startNum + 5;
+
+            Response.slice(startNum, endNum).map((values) => {
                 data = `
                     <p class="reveal">
                         <br><strong>“${values.content}”</strong>
                         <br><em>${values.author}</em>
                     </p>
                 `
+                document.getElementById(postContainerElementID).innerHTML += data;
             })
-            document.getElementById(postContainerElementID).innerHTML = data;
         });
 }
-
 loadQuotes('quotes');
