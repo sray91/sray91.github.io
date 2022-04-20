9f6d6f130acd05eb06ce415cb2696092101acd71
swanagan <swanaganray@gmail.com>
2022-04-19 22:47:31 -0400

    stuffing the turkey

--- js/detailFunction.js
+++ js/detailFunction.js
@@ -1,18 +1,4 @@
 function detailFunction(prop) {
     document.getElementById(prop).className = "detail-box";
-
-    fetch("../content/resume.json")
-    .then(Response => Response.json()) 
-    .then(Response => {
-        Response.map((values) => {
-            data = `
-                <p>
-                    <br><strong>“${values.company}”</strong>
-                    <br><strong>"${values.title}"</strong>
-                    <br>${values.content}
-                </p>
-            `
-            document.getElementById(prop).innerHTML += data;
-        })
-    });
+    document.getElementById(prop).innerHTML = "";
 }
